e1fdf3e33c0d017dd2c785853a93e5a41bfc682a
William James <52788612+william-james-pj@users.noreply.github.com>
2023-03-25 15:16:35 -0300

    refactor: Moved color and mixin variables to separate files from global

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
