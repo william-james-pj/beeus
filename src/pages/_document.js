5b2c17996955acd0e1f800877008aa18840f2e6a
William James <52788612+william-james-pj@users.noreply.github.com>
2023-03-25 13:49:49 -0300

    refactor: Moved common configuration settings to parent class

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Beeus" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>Beeus</title>
-        <meta name="description" content="Beeus" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main></main>
     </>
