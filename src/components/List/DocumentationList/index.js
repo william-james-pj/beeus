9c70f8b869073997045d92950a4953c8198c7610
William James <52788612+william-james-pj@users.noreply.github.com>
2023-06-04 20:15:23 -0300

    feat: add load

--- src/components/List/DocumentationList/index.js
+++ src/components/List/DocumentationList/index.js
@@ -1,12 +1,13 @@
 import { useAuth } from '@/contexts/authContext';
 import { useDocumentation } from '@/hooks/useDocumentationList';
 import { useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa';
 import { DocumentationCard } from '../DocumentationCard';
 import styles from './documentationList.module.scss';
 
 export function DocumentationList() {
   const { userToken } = useAuth();
-  const { documents, getDocumentations } = useDocumentation();
+  const { isLoading, documents, getDocumentations } = useDocumentation();
 
   useEffect(() => {
     if (!userToken) return;
@@ -17,9 +18,13 @@ export function DocumentationList() {
     <div className={styles.container}>
       <h1 className={styles.title}>Artigos</h1>
       <div className={styles.content}>
-        {documents?.map((item, index) => (
-          <DocumentationCard key={index} data={item} />
-        ))}
+        {isLoading ? (
+          <FaSpinner className={styles.spinner} size={32} />
+        ) : (
+          documents?.map((item, index) => (
+            <DocumentationCard key={index} data={item} />
+          ))
+        )}
       </div>
     </div>
   );
--- src/hooks/useDocumentationList.js
+++ src/hooks/useDocumentationList.js
@@ -7,24 +7,32 @@ import { useState } from 'react';
 export function useDocumentation() {
   const [documents, setDocuments] = useState([]);
   const [selectedDocument, setSelectedDocument] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getDocumentations({ token }) {
+    setIsLoading(true);
+
     const { status, data } = await listDocumentationRequest({ token });
 
     if (status && !data) return;
 
     setDocuments(data);
+    setIsLoading(false);
   }
 
   async function getDocumentationById({ id, token }) {
+    setIsLoading(true);
+
     const { status, data } = await getDocumentationByIdRequest({ id, token });
 
     if (status && !data) return;
 
     setSelectedDocument(data);
+    setIsLoading(false);
   }
 
   return {
+    isLoading,
     documents,
     selectedDocument,
     getDocumentations,
